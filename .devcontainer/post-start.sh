a5c19413ae423a62a8353b068cbb73cd6ce4da28
tjarbo <16938041+tjarbo@users.noreply.github.com>
2026-03-23 06:36:19 +0000

    chore: update devcontainer to v2

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,25 +1,5 @@
 #!/bin/sh
 # Plugin specific post-start.sh script.
 
-CFG_FILE="./.devcontainer/pretalx.cfg"
-TEMPLATE_FILE="./.devcontainer/pretalx.cfg.template"
-
-GREEN="\033[1;32m"
-YELLOW="\033[1;33m"
-RED="\033[1;31m"
-RESET="\033[0m"
-
-echo "${YELLOW}ℹ Current working directory: $(pwd)${RESET}"
-
-if [ -f "$CFG_FILE" ]; then
-  echo "${GREEN}✔ Configuration file '$CFG_FILE' already exists.${RESET}"
-else
-  if [ -f "$TEMPLATE_FILE" ]; then
-    cp "$TEMPLATE_FILE" "$CFG_FILE"
-    echo "${GREEN}⚡ Created '$CFG_FILE' from template.${RESET}"
-  else
-    echo "${RED}✖ Template file '$TEMPLATE_FILE' not found. Cannot create '$CFG_FILE'.${RESET}"
-    exit 1
-  fi
-fi
-
+echo "Nothing to do";
+exit 0;
